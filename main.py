f1218257e19f2b501c2d1f11cf8ba2f270ba9c12
Tavleenkm <tavleenmashiana@gmail.com>
2023-09-18 00:12:12 -0500

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from redditscrape import HtmlDownloader
 
 if __name__ == "__main__":
+    url = input("Enter the URL: ").strip()  # Prompt for the URL input
     downloader = HtmlDownloader()
-    url = input("Enter the URL: ").strip()
     downloader.download_html_from_url(url)
-
