31c94649bccf915c5f767532f48f4c00164426ec
Tavleenkm <tavleenmashiana@gmail.com>
2023-09-18 00:05:51 -0500

    First Commit

--- redditscrape.py
+++ redditscrape.py
@@ -2,30 +2,28 @@ import os
 import requests
 from urllib.parse import urlparse
 
-class HtmlDownloader:
-    def __init__(self):
-        self.output_dir = "html_files"
-        os.makedirs(self.output_dir, exist_ok=True)
+def download_html_from_url(url):
+    # Add 'https://' prefix if missing
+    if not urlparse(url).scheme:
+        url = 'https://' + url
 
-    def download_html_from_url(self, url):
-        # Add 'https://' prefix if missing
-        if not urlparse(url).scheme:
-            url = 'https://' + url
+    # Add 'www' prefix if missing
+    parsed_url = urlparse(url)
+    if not parsed_url.netloc.startswith('www.'):
+        url = parsed_url.scheme + '://www.' + parsed_url.netloc + parsed_url.path
 
-        # Add 'www' prefix if missing
-        parsed_url = urlparse(url)
-        if not parsed_url.netloc.startswith('www.'):
-            url = parsed_url.scheme + '://www.' + parsed_url.netloc + parsed_url.path
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
 
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
+        # Save the raw HTML content to a text file
+        with open('output.txt', 'w', encoding='utf-8') as file:
+            file.write(response.text)
 
-            # Extract a filename from the path (e.g., "15hf4vr_egypt_changed_my_perspective_on_travel.html")
-            filename = os.path.join(self.output_dir, parsed_url.path.lstrip('/').replace('/', '_') + '.html')
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(response.text)
+        print(f"HTML content downloaded and saved to 'output.txt'")
+    except Exception as e:
+        print(f"Error: {e}")
 
-            print(f"HTML content downloaded and saved to '{filename}'")
-        except Exception as e:
-            print(f"Error: {e}")
+if __name__ == "__main__":
+    url = input("Enter the URL: ").strip()  # Prompt for the URL input
+    download_html_from_url(url)
