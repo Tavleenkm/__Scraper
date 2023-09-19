16111bb19679e5ec5319f86eec9fffdfde56b038
Tavleenkm <tavleenmashiana@gmail.com>
2023-09-18 22:44:53 -0500

    modified code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from redditscrape import HtmlDownloader
+import os
+from redditscrape import download_html_from_url
 
 if __name__ == "__main__":
     url = input("Enter the URL: ").strip()  # Prompt for the URL input
-    downloader = HtmlDownloader()
-    downloader.download_html_from_url(url)
+    html_content = download_html_from_url(url)
+
+    if html_content is not None:
+        # Save the raw HTML content to a text file
+        with open('output.txt', 'w', encoding='utf-8') as file:
+            file.write(html_content)
+
+        print("HTML content downloaded and saved to 'output.txt'")
